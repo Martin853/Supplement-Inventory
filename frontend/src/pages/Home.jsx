6ba45ce6fd4ec47a0622f51dfa841dd77e713cc1
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-10 18:59:07 +0200

    Created Product componen and added loading scree

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { Product } from "../components/Product";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export const Home = () => {
   // Products
@@ -21,9 +23,17 @@ export const Home = () => {
   }, []);
 
   return (
-    <div>
-      {products &&
-        products.map((product) => <p key={product._id}>{product.title}</p>)}
+    <div className='w-full p-4 grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3'>
+      {products ? (
+        products.map((product) => (
+          <Product key={product._id} product={product} />
+        ))
+      ) : (
+        <div className='flex flex-col justify-center items-center gap-3 h-screen'>
+          <h1 className='text-4xl'>Loading...</h1>
+          <AiOutlineLoading3Quarters className='animate-spin text-4xl ' />
+        </div>
+      )}
     </div>
   );
 };
