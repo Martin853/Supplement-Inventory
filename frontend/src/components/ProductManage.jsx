b4452b00e91e377894af6344990de3d2c2787467
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-11 15:10:05 +0200

    Added buttons to product manage

--- frontend/src/components/ProductManage.jsx
+++ frontend/src/components/ProductManage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 
 export const ProductManage = ({ product }) => {
   const baseURL = window.location.origin;
@@ -19,6 +20,16 @@ export const ProductManage = ({ product }) => {
       <h1>Price: {product.price}$</h1>
       <h1>Quantity: {product.quantity}</h1>
       <h1>{product.createdAt}</h1>
+      <div className='w-full h-fit py-2 flex gap-2'>
+        <button className='w-full flex justify-center items-center gap-2 bg-emerald-400 border-emerald-500 border-2 p-2 rounded-lg text-white font-semibold hover:bg-emerald-500 transition-all ease-in-out duration-300'>
+          <AiFillEdit />
+          Edit
+        </button>
+        <button className='w-full flex justify-center items-center gap-2 bg-red-400 border-red-500 border-2 p-2 rounded-lg text-white font-semibold hover:bg-red-500 transition-all ease-in-out duration-300'>
+          <AiFillDelete />
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
