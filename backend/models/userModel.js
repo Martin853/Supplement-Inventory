14b49ff1870003545609c79f1dd56696bef9d070
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-13 20:54:10 +0200

    Added validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -18,6 +19,20 @@ const userSchema = new Schema({
 // Static Signup Method
 
 userSchema.statics.signup = async function (email, password) {
+  // Validation
+
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Enter a valid email");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough");
+  }
+
   const exists = await this.findOne({ email });
 
   if (exists) {
