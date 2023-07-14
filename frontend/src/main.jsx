60b818ac2226ea1dae5ca682f19f3ef12445338f
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-14 14:05:25 +0200

    Created auth context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
 import { ProductsContextProvider } from "./context/ProductContext.jsx";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ProductsContextProvider>
-    <App />
-  </ProductsContextProvider>
+  <AuthContextProvider>
+    <ProductsContextProvider>
+      <App />
+    </ProductsContextProvider>
+  </AuthContextProvider>
 );
